cbc07b7c81fb83d0b2e6832442ab31aea1ee3235
Merlin <merlinio2000@users.noreply.github.com>
2016-10-27 13:57:10 +0200

    Create Participant.java
    Helper Class for rows in Array

---
+++ Participant.java
@@ -0,0 +1,21 @@
+class Participant
+{
+	private int startNr		  =  0;
+	private int category	  =  0;
+	private String name		  = "";
+	private String nameNr 	  = "";
+  
+	public Participant(String[] s)
+	{
+		this.startNr	= Integer.parseInt(s[0]);
+		this.category	= Integer.parseInt(s[1]);
+		this.name		= s[2];
+		this.nameNr = s[3];
+	}
+	
+	public int		getStartNr()	{return startNr; }
+	public int		getCategory()	{return category;}
+	public int		getName()		{return name;	 }
+	public String	getNameNr()		{return nameNr;  }
+	
+}
