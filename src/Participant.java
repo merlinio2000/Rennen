4cd59710ce5edd969b49f5448a002cc35b31b2ff
Merlin <merlin@merlinio.ch>
2016-10-27 15:24:20 +0200

    Signed-off-by: Merlin <merlin@merlinio.ch>

--- src/Participant.java
+++ src/Participant.java
@@ -1,21 +1,26 @@
 class Participant
 {
 	private int startNr	  =  0;
-	private int category	  =  0;
+	private int category  =  0;
 	private String name	  = "";
-	private String nameNr 	  = "";
+	private String nameNr = "";
   
 	public Participant(String[] s)
 	{
 		this.startNr	= Integer.parseInt(s[0]);
 		this.category	= Integer.parseInt(s[1]);
-		this.name	= s[2];
+		this.name		= s[2];
 		this.nameNr 	= s[3];
 	}
 	
-	public int	getStartNr()	{return startNr; }
-	public int	getCategory()	{return category;}
-	public int	getName()	{return name;	 }
-	public String	getNameNr()	{return nameNr;  }
+	public int		getStartNr()	{return startNr; }
+	public int		getCategory()	{return category;}
+	public String	getName()		{return name;	 }
+	public String	getNameNr()		{return nameNr;  }
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
 	
 }
