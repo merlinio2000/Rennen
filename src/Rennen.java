a27753499093ff72f7588dc786dd55a8b2d49498
merlinio2000 <merlin@merlinio.ch>
2016-11-02 17:42:09 +0100

    Signed-off-by: merlinio2000 <merlin@merlinio.ch>

--- src/Rennen.java
+++ src/Rennen.java
@@ -26,13 +26,15 @@ public class Rennen
 					pr.add(new ParticipantResult(p, r));
 			}
 		}
+		// create 3 lists with the different categories
 		List<ParticipantResult> men = pr.stream().filter(p -> p.getCategory() == 1).collect(Collectors.toList());
 		List<ParticipantResult> women = pr.stream().filter(p -> p.getCategory() == 2).collect(Collectors.toList());
 		List<ParticipantResult> prof = pr.stream().filter(p -> p.getCategory() == 3).collect(Collectors.toList());
 		
+		//sort the list by the time difference ascending
 		men.sort((p1, p2) -> Long.compare(p1.getDifference(), p2.getDifference()));
-		
-		
+		women.sort((p1, p2) -> Long.compare(p1.getDifference(), p2.getDifference()));
+		prof.sort((p1, p2) -> Long.compare(p1.getDifference(), p2.getDifference()));
 		
 		/*for (ParticipantResult p: pr)
 		{
