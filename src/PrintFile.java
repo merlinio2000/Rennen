f846c1537a094c766b789238348b4f489cf1ee07
merlinio2000 <merlin@merlinio.ch>
2016-11-02 18:00:03 +0100

    Signed-off-by: merlinio2000 <merlin@merlinio.ch>

---
+++ src/PrintFile.java
@@ -0,0 +1,18 @@
+/**
+ * Helper class to print the results into a file
+ * @author Merlin Maggi, Dogukan Kabagoez, Florian Siegrist
+ * @version 1.0
+ */
+public class PrintFile 
+{
+	private String fileName = "";
+	
+	/**
+	 * constructs and sets the fileName
+	 * @param _fileName  the name of the file
+	 */
+	public PrintFile(String _fileName)
+	{
+		this.fileName = _fileName;
+	}
+}
